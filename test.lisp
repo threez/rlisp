a0ce44bc0793be80b0614d9c0200fb30fbec4af3
Vincent Landgraf <setcool@gmx.de>
2010-03-12 00:07:33 +0100

    changed test to use short atom names

--- test.lisp
+++ test.lisp
@@ -1,28 +1,28 @@
 (import common)
 
 ;; Addiere 2 und 2:
-(p (add (div 8 2) (mult (add 2 2) (sub 10 5))))
-(p (add 2 2))
-(p (sub 8 (add 2 4)))
-(p (mult 2 2))
+(p (+ (/ 8 2) (* (+ 2 2) (- 10 5))))
+(p (+ 2 2))
+(p (- 8 (+ 2 4)))
+(p (* 2 2))
 
 ;; lambda & label
-(p ((lambda (x) (mult x x)) 5))
+(p ((lambda (x) (* x x)) 5))
 
-(label test (lambda (a b) (add a b)))
+(label test (lambda (a b) (+ a b)))
 
 (p (test 10 20))
 
-(p ((lambda (a b) (add a b)) 1 2))
+(p ((lambda (a b) (+ a b)) 1 2))
 
-(p ((lambda (a b) ((lambda (a b) (add a b)) a b)) 1 2))
+(p ((lambda (a b) ((lambda (a b) (+ a b)) a b)) 1 2))
  
 ;; Setze die Variable p auf den Wert 3,1415:
 (setq pi 3.1415)
  
 ;; Definiere eine Funktion, die ihr Argument quadriert:
 (defun square (x)
-  (mult x x))
+  (* x x))
  
 ;; Quadriere die Zahl 3:
 (square 3)
@@ -30,10 +30,10 @@
 
 ;; Fibbonacci
 (defun fib (n)
-  (cond (lt n 2)
+  (cond (< n 2)
     n
-    (add (fib (sub n 1)) 
-         (fib (sub n 2)))))
+    (+ (fib (- n 1)) 
+         (fib (- n 2)))))
          
 (p (fib 0))
 (p (fib 1))
@@ -45,4 +45,6 @@
 (p (fib 15))
 (p (fib 20))
          
-(p (cond (gt 1 2) (quote T) (quote ())))
+(p (cond (> 1 2) (quote T) (quote ())))
+
+(print Hello World)
